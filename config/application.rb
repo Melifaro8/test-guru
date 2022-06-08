e3f327ed54e80cdf34e9bcf91fb8ee47ba5f50b5
Melifaro8 <vipolynsky@yandex.ru>
2022-06-08 18:07:02 +0300

    noopener nofollow local static page

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    I18n.locale = "en"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
