1bbef6f717d2407492a908fb668543c57752a292
Melifaro8 <vipolynsky@yandex.ru>
2022-07-19 21:52:53 +0300

    XZ

--- db/migrate/20220621112831_create_tests.rb
+++ db/migrate/20220621112831_create_tests.rb
@@ -3,6 +3,11 @@ class CreateTests < ActiveRecord::Migration[7.0]
     create_table :tests do |t|
       t.string :title
       t.integer :level
+<<<<<<< HEAD
+      t.references :author, null: false, foreign_key: { to_table: :users }
+      t.references :category, null: false, foreign_key: true
+=======
+>>>>>>> 5e3a7feb6babc0e17ac7344bcd33fc0ac835a734
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,40 +10,68 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
+ActiveRecord::Schema[7.0].define(version: 2022_06_22_081827) do
+=======
 ActiveRecord::Schema[7.0].define(version: 2022_06_21_113440) do
+>>>>>>> 5e3a7feb6babc0e17ac7344bcd33fc0ac835a734
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
+<<<<<<< HEAD
+    t.string "body", null: false
+    t.integer "question_id", null: false
+=======
     t.string "body"
     t.integer "question_id"
+>>>>>>> 5e3a7feb6babc0e17ac7344bcd33fc0ac835a734
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
+<<<<<<< HEAD
+    t.string "title", null: false
+=======
     t.string "title"
+>>>>>>> 5e3a7feb6babc0e17ac7344bcd33fc0ac835a734
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
+<<<<<<< HEAD
+    t.string "body", null: false
+    t.integer "test_id", null: false
+=======
     t.string "body"
     t.integer "test_id"
+>>>>>>> 5e3a7feb6babc0e17ac7344bcd33fc0ac835a734
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "tests", force: :cascade do |t|
+<<<<<<< HEAD
+    t.string "title", null: false
+    t.integer "level", null: false
+=======
     t.string "title"
     t.integer "level"
+>>>>>>> 5e3a7feb6babc0e17ac7344bcd33fc0ac835a734
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
+<<<<<<< HEAD
+    t.string "login", null: false
+    t.string "password", null: false
+=======
     t.string "login"
     t.string "password"
+>>>>>>> 5e3a7feb6babc0e17ac7344bcd33fc0ac835a734
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
