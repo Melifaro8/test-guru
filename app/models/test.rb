c1f4b2f36e98f530f915b74b438105728754a15c
Melifaro8 <vipolynsky@yandex.ru>
2022-08-07 11:44:45 +0300

    add model result and changes to User and Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,4 @@
 class Test < ApplicationRecord
+  def sort_test_by_category(category_title)
+    Tset.joins(:test).where(categories: {title: category_title}).order(title: :desk).pluck(:title)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+  def test_history(level)
+    Test.joins('JOIN results ON results.test_id = test.id').where(:level => level).where(results: {user_id => user.id})
+  end
+    
+    # написать метод который будет формировать SQL запрос и отправлять его на вывод (объединить таблицы)
 end
